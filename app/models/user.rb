befd1e8a0a20346bb33dc7a0000a6a18f3d08e7a
Andre Menezes <djdedemenezes@gmail.com>
2021-08-27 21:47:00 -0300

    bundled pundit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :friendships, dependent: :destroy
+has_many :friendships, dependent: :destroy
   has_many :friends, through: :friendships
   has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id"
   has_many :is_friend, through: :inverse_friendships, source: :user
